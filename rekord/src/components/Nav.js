3bcd37a7db1812632a1e390157a142b6f5688dab
Lee Len Formento <leebatef@gmail.com>
2018-12-15 18:12:54 -0500

    deleting some comments

--- rekord/src/components/Nav.js
+++ rekord/src/components/Nav.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import logo from './assets/logo2.png'
 import './styles/styles.css';
-// import { BrowserRouter as Router } from 'react-router';
-// import { NavLink } from 'react-router-dom';
-// import Signin from './Signin';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
@@ -11,9 +8,6 @@ const Nav = () => {
         <div className="nav-items-container">
         <img className="logo" src={logo} alt="Logo" />
         <div className="nav-items">
-        {/* <NavLink to="/signin">LOG IN  &nbsp; &nbsp;| </NavLink>
-        <NavLink to="/signup"> &nbsp; &nbsp;REGISTER</NavLink> */}
-        {/* <Route path="/signin" component={Signin} /> */}
         <Link to={"/signin"}> LOG IN &nbsp;| &nbsp;</Link>
         <Link to={"/signup"}> REGISTER &nbsp; </Link>
 
